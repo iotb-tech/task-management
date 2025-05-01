6eaebab7505de8a8e2a7c083b412c21c6dd26050
obayomiayomide <obayomiayomide2020@gmail.com>
2025-05-01 20:58:54 +0100

    mistake

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="flex">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Dashboard;
