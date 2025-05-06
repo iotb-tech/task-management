0397eb4cad2f4e0765f4f8c3204d5f4c49a7660e
obayomiayomide <obayomiayomide2020@gmail.com>
2025-05-06 16:37:54 +0100

    app to avoid conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import "./App.css";
-import Dashboard from "./pages/Dashboard";
-import { ContextProvider } from "./components/ContextProvider";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div>
-        <ContextProvider>
-          <Router>
-            <Dashboard />
-          </Router>
-        </ContextProvider>
-      </div>
+      <div></div>
     </>
   );
 }
