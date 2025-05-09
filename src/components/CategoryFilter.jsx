e5d8ef2dff208866a9b7f724bd526af0c7566ccb
Waliyat <waliyatkehinde@gmail.com>
2025-05-09 22:38:19 +0100

    update the darkmode of the task list page

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,20 +1,24 @@
+import { useAppContext } from "./ContextProvider";
 
-const CategoryFilter = ({ categories, categoryFilter, setCategoryFilter }) => (
-    <div className="flex flex-wrap gap-2 mb-4">
-        {categories.map((category) => (
-        <button
-            key={category}
-            onClick={() => setCategoryFilter(category)}
-            className={`px-4 py-2 rounded shadow-sm transition border cursor-pointer ${
-            categoryFilter === category
-                ? "ring-2 ring-[#287291] text-gray-700 font-semibold"
-                : "primary-color hover:bg-[#19475b]"
-            }`}
-        >
-            {category}
-        </button>
-        ))}
-    </div>
-);
+const CategoryFilter = ({ categories, categoryFilter, setCategoryFilter }) => {
+    const {isLightMode} = useAppContext();
+    return (
+        <div className="flex flex-wrap gap-2 mb-4">
+            {categories.map((category) => (
+            <button
+                key={category}
+                onClick={() => setCategoryFilter(category)}
+                className={`px-4 py-2 rounded shadow-sm transition border cursor-pointer ${
+                categoryFilter === category
+                    ? "ring-2 ring-[#287291] text-gray-700 font-semibold"
+                    : "primary-color hover:bg-[#19475b]"
+                } ${isLightMode ? "" : "text-white"}`}
+            >
+                {category}
+            </button>
+            ))}
+        </div>
+    );
+};
 
 export default CategoryFilter;
--- src/components/TaskHeader.jsx
+++ src/components/TaskHeader.jsx
@@ -1,16 +1,21 @@
+import { useAppContext } from "./ContextProvider";
 
-const TaskHeader = ({ formattedDate, formattedTime, weatherInfo }) => (
-    <div className="primary-text">
-        <h1 className="text-4xl font-bold primary-font text-gray-800 mb-2">
-            Hi DevCode
-        </h1>
-        <p className="text-gray-600">
-            {formattedDate} | {formattedTime}
-        </p>
-        <p className="text-gray-600">
-            Weather in Lagos: {weatherInfo.condition}, {weatherInfo.temperature} - {weatherInfo.forecast}
-        </p>
-    </div>
-);
+const TaskHeader = ({ formattedDate, formattedTime, weatherInfo }) => {
+    const {isLightMode} = useAppContext();
+
+    return (
+        <div className="primary-text">
+            <h1 className={`text-4xl font-bold primary-font mb-2 ${isLightMode ? "text-gray-800" : "text-white"}`}>
+                Hi DevCode
+            </h1>
+            <p className={`${isLightMode ? "text-gray-600" : "text-white"}`}>
+                {formattedDate} | {formattedTime}
+            </p>
+            <p className={`${isLightMode ? "text-gray-600" : "text-white"}`}>
+                Weather in Lagos: {weatherInfo.condition}, {weatherInfo.temperature} - {weatherInfo.forecast}
+            </p>
+        </div>
+    );
+};
 
 export default TaskHeader;
